6b1a27042ee7b8ff1b32b452a624493db8a4088f
Savath Saypadith <mr.saypadith@gmail.com>
2023-10-05 09:35:14 +0700

    add 2com1 to branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! 2IT3");
+        System.out.println("Hello World! 2COM1");
     }
 }
