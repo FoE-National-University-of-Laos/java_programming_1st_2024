f700e73fdedb486d04463b985e8845f5ab286793
Savath Saypadith <mr.saypadith@gmail.com>
2023-10-05 09:38:20 +0700

    add ceit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! 2COM1");
+        System.out.println("Hello World! 2COM1 from CEIT");
     }
 }
