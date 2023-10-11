fc8dd3253b9a90b200fa7e3eeb79906c91ed1b41
Savath Saypadith <mr.saypadith@gmail.com>
2023-10-11 10:52:46 +0700

    chapter 2 installation

---
+++ CEIT-Java/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static int age = 18;
+    static double height = 1.75;
+    static char title = 'A';
+    static boolean isStudent = true;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Age: "+ age);
+        System.out.println("Height: "+ height);
+        System.out.println("Title: "+ title);
+        System.out.println("Is student: "+ isStudent);
+    }
+
+}
