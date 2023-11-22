8ae459f533d248ba9eff1b72c1f6565801868882
Savath Saypadith <mr.saypadith@gmail.com>
2023-11-22 14:27:40 +0700

    add input stream

--- CEIT-Java/src/student/exam/Grade.java
+++ CEIT-Java/src/student/exam/Grade.java
@@ -1,4 +1,36 @@
 package student.exam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Grade {
+    public String calculate(int point) {
+        if (point >= 80) {
+            return "A";
+        } else if (point >= 70) {
+            return "B";
+        } else if (point >= 60) {
+            return "C";
+        } else if (point >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String s;
+        InputStreamReader ir=new InputStreamReader(System.in);
+        BufferedReader in= new BufferedReader(ir);
+        System.out.print("Please enter your withdraw amount:");
+        try{
+            s=in.readLine();
+            System.out.println("You have withdrawn " + s + " KIP");
+        }catch(IOException e){
+            System.out.println("Read input keybroad error");
+        }
+
+    }
 }
--- CEIT-Java/src/student/register/Home.java
+++ CEIT-Java/src/student/register/Home.java
@@ -1,4 +1,10 @@
 package student.register;
 
+import student.exam.Grade;
+
 public class Home {
+    public static void main(String[] args) {
+        Grade grade = new Grade();
+        System.out.println(grade.calculate(80));
+    }
 }
