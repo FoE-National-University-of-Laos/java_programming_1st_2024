ec79676a5c2090b6ae360be7f27c3ef4e327d639
Savath Saypadith <mr.saypadith@gmail.com>
2023-11-29 09:35:35 +0700

    add operator and operant

---
+++ CEIT-Java/src/VariableExample.java
@@ -0,0 +1,13 @@
+
+public class VariableExample {
+    static int age;
+
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(++a); // 1
+        a = 2;
+        System.out.println(a++); // 2
+        System.out.println(a); // 3
+//        System.out.println("Age: "+ age);
+    }
+}
