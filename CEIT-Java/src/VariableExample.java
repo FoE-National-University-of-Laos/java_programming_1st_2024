e9f52190bea561b126fef65e0009094244264dab
Savath Saypadith <mr.saypadith@gmail.com>
2023-11-29 14:44:35 +0700

    add constant example

---
+++ CEIT-Java/src/VariableExample.java
@@ -0,0 +1,13 @@
+public class VariableExample {
+
+
+    public static void main(String[] args) {
+        System.out.println(3*(-5-(7%4/2)+26*2+4));
+//        int a=0;
+//        System.out.println(++a); // 1
+//        a=2;
+//        System.out.println(a++); // 2
+//        System.out.println(a); // 3
+
+    }
+}
