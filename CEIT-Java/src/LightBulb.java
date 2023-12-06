f44c7367bf47c0d7893ce50eec8af292d3718ebd
Savath Saypadith <mr.saypadith@gmail.com>
2023-12-06 15:16:25 +0700

    add class example

---
+++ CEIT-Java/src/LightBulb.java
@@ -0,0 +1,34 @@
+public class LightBulb {
+    // these are the attributes of the light bulb class
+    static String color;
+    static int watts;
+    static boolean indicator;
+    static int brightness;
+    static private int state;
+    static String model;
+    static String brand;
+    static String type;
+    static String shape;
+    static String size;
+
+    // these are the methods of the light bulb class
+    public static void turnOn() {
+        state = 1;
+        System.out.println("The light bulb is on.");
+    }
+
+    public static void turnOff() {
+        state = 0;
+        System.out.println("The light bulb is off.");
+    }
+
+    public static void adjustBrightness(int brightness) {
+        System.out.println("The brightness is " + brightness + ".");
+    }
+
+    // this is the main method
+    public static void main(String[] args) {
+        System.out.println("This is my light bulb!");
+        turnOn();
+    }
+}
--- CEIT-Java/src/VariableExample.java
+++ CEIT-Java/src/VariableExample.java
@@ -2,12 +2,7 @@ public class VariableExample {
 
 
     public static void main(String[] args) {
-        System.out.println(3*(-5-(7%4/2)+26*2+4));
-//        int a=0;
-//        System.out.println(++a); // 1
-//        a=2;
-//        System.out.println(a++); // 2
-//        System.out.println(a); // 3
-
+        LightBulb lightBulb = new LightBulb();
+        lightBulb.turnOn();
     }
 }
