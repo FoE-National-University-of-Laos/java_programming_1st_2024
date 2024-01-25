fcfbdbfcd6d02e0fb6a82cadc9848cdbff2ec6a6
Savath Saypadith <mr.saypadith@gmail.com>
2024-01-25 09:47:03 +0700

    add loop while and infinite loop

--- CEIT-Java-2com1/src/BookDisplay.java
+++ CEIT-Java-2com1/src/BookDisplay.java
@@ -1,8 +1,49 @@
 public class BookDisplay {
     public static void main(String[] args) {
-        Book book = new Book();
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, 10.99);
-        book.displayBookDetails();
-        book1.displayBookDetails();
+        int a = 1;
+//        while (a <= 10) {
+//            System.out.println(a);
+//            a++;
+//        }
+//        do {
+//            System.out.println(a);
+//            a++;
+//        } while (a <= 10);
+//        System.out.println("last:"+a);
+
+//        Book book = new Book();
+//        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, 10.99);
+//        book.displayBookDetails();
+//        book1.displayBookDetails();
+//        int year = 1970;
+//        for (int i = 2; i <= 10; i++) {
+//            for (int j = 1; j <= 15; j++) {
+//                System.out.println(i + " x " + j + " = " + (i * j));
+//            }
+//        }
+
+//        for (int i = 2; i <= 10; i++) {
+//            System.out.println(i);
+//            if (i % 2 == 0) {
+//                continue;
+//            }
+//        }
+
+        // continue; keyword example
+//        int year = 2000;
+//        while (year <= 2021) {
+//            year++;
+//            if (year % 2 == 0) {
+//                continue;
+//            }
+//            System.out.println(year);
+//        }
+
+        // infinite loop
+//        for (int i = 0; i >= 0; i++) {
+//            System.out.println(i);
+//        }
+        
+//        System.out.println("last:"+year);
     }
 }
